dcd47ae4fb37973c9c4748fac11f3916189c0a36
JUSTIN_WANG <justin.wang1ab@gmail.com>
2024-08-03 12:22:04 -0400

    change update function

--- src/paddle.py
+++ src/paddle.py
@@ -1,10 +1,12 @@
-import numpy as np
+import cv2 as cv
 import pygame as pg
-import math
+import numpy as np
+import mediapipe as mp
 
 class Paddle:
     def __init__(self, center: tuple = (0,0), height: float = 60, width: float = 15, sprite_path: str = ""):
         self.vel = np.zeros(2, np.float32)
+        self.angle = 0
         self.ang_vel = np.zeros(2, np.float32)
         self.center = np.array(center, np.float32)
 
@@ -23,24 +25,44 @@ class Paddle:
         else:
             self.sprite = pg.transform.smoothscale(pg.image.load(sprite_path), (self.width, self.height))
         
-    """
-    new_box is a tuple of 4 tuples arranged such that:
-        new_box[0] is the top left corner of the paddle
-        new_box[1] is the top right corner of the paddle
-        new_box[2] is the bottom left corner of the paddle
-        new_box[3] is the bottom right corner of the paddle
-    """
-    def update(self, dt: float, new_box: tuple):
-        top_left = np.array(new_box[0])
-        top_right = np.array(new_box[1])
-        bot_left = np.array(new_box[2])
-        bot_right = np.array(new_box[3])
-        center = (top_left + bot_right) / 2
+    def find_closest_face(self, frame: np.array, face_detection):
+        results = face_detection.process(frame)
+
+        if (results.detections):
+            distances = []
+            for detection in results.detections:
+                key_points = np.array([(p.x, p.y) for p in detection.location_data.relative_keypoints]) 
+                key_points_coords = np.multiply(key_points,[1920,1080],).astype(int)
+                eye_points = key_points_coords[0:2]
+                midpoint = (eye_points[0] + eye_points[1]) / 2
+                distances.append(np.linalg.norm(self.center - midpoint))
+            
+            shortest_distance = min(distances)
+
+            return results.detections[distances.index(shortest_distance)]
+
+    def rotation_matrix(self, angle: float):
+        return np.array([
+            [np.cos(angle), -np.sin(angle)],
+            [np.sin(angle), np.cos(angle)]
+        ])
+
+    def update(self, dt: float, capture: cv.VideoCapture, face_detection):
+        frame = capture.read()
+        grayscale = cv.cvtColor(frame[:, ::-1], cv.COLOR_BGR2GRAY)
+        face = self.find_closest_face(grayscale, face_detection)
+
+        eye_points = np.array([(p.x * 1920, p.y * 1080) for p in face.location_data.relative_keypoints][0:2])
+        angle_from_horizontal = np.arctan2(eye_points[0][1] - eye_points[1][1], eye_points[0][0] - eye_points[1][0])
+        midpoint = (eye_points[0] + eye_points[1]) / 2
+        rotation_matrix = self.rotation_matrix(angle_from_horizontal)
+        top_left = np.dot(rotation_matrix, np.array([midpoint[0] - self.width / 2, midpoint[1] - self.hieght / 2]))
+        top_right = np.dot(rotation_matrix, np.array([midpoint[0] + self.width / 2, midpoint[1] - self.height / 2]))
+        bot_left = np.dot(rotation_matrix, np.array([midpoint[0] - self.width / 2, midpoint[1] + self.height / 2]))
+        bot_right = np.dot(rotation_matrix, np.array([midpoint[0] - self.width / 2, midpoint[1] + self.height / 2]))
 
         # compute rotation angle
-        original_vec = self.corners["top_left"] - self.center
-        new_vec = top_left - center
-        angle = math.atan2(original_vec[1], original_vec[0]) - math.atan2(new_vec[1], new_vec[0])
+        angle = angle_from_horizontal - self.angle
         self.ang_vel = angle / dt
 
         self.corners["top_left"] = top_left
@@ -48,8 +70,9 @@ class Paddle:
         self.corners["bot_left"] = bot_left
         self.corners["bot_right"] = bot_right
 
-        self.vel = (center - self.center) / dt
-        self.center = center
+        self.vel = (midpoint - self.center) / dt
+        self.center = midpoint
+        self.angle = angle_from_horizontal
     
     def compute_transferred_velocity(self, point_of_collision: tuple, xoffset: float, yoffset: float):
         tangent_vel = self.ang_vel * np.linalg.norm(self.center - np.array(point_of_collision)) * np.sign(np.array([xoffset, yoffset]))
